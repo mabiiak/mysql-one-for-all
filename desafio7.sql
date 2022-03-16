fc382539bf8e4010cd7301665b79d3abf9c035f7
mabiak <mabiane.polniak@gmail.com>
2022-03-16 10:38:56 -0300

    requisito 7 - concluido

---
+++ desafio7.sql
@@ -0,0 +1,13 @@
+SELECT 
+	art.nome_artista AS artista,
+    alb.nome_albuns AS album,
+    COUNT(follow.id_artista) AS seguidores
+    
+FROM SpotifyClone.artista AS art
+INNER JOIN SpotifyClone.albuns AS alb
+ON art.id_artista = alb.id_artista
+
+INNER JOIN SpotifyClone.relacao_seguidores AS follow
+ON follow.id_artista = art.id_artista
+GROUP BY art.nome_artista, alb.nome_albuns, follow.id_artista
+ORDER BY seguidores DESC
